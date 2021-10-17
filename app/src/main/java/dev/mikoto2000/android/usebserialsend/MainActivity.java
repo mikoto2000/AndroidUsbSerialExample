0625dccace3b5533eb0d536b282ad2c2cada1505
mikoto2000 <mikoto2000@gmail.com>
2021-10-18 00:25:10 +0900

    Added empty activity project.

---
+++ app/src/main/java/dev/mikoto2000/android/usebserialsend/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.mikoto2000.android.usebserialsend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
